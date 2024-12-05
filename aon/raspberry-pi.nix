6d56dced03029278ce98559e11798cc256d8e8b2
guillem.cordoba <guillem.cordoba@gmail.com>
2024-12-05 23:12:29 +0100

    Avahi

--- aon/raspberry-pi.nix
+++ aon/raspberry-pi.nix
@@ -46,6 +46,18 @@
               };
             };
 
+            services.avahi = {
+              nssmdns4 = true;
+              enable = true;
+              ipv4 = true;
+              ipv6 = true;
+              publish = {
+                enable = true;
+                addresses = true;
+                workstation = true;
+              };
+            };
+
             users = {
               mutableUsers = false;
               users."${user}" = {
