f78fd34ec0ae5fd9bdb4ca97b2cc2ebc75c18db8
guillem.cordoba <guillem.cordoba@gmail.com>
2025-06-16 15:36:37 +0200

    Add locker

--- flake.nix
+++ flake.nix
@@ -26,6 +26,7 @@
 
     push-notifications-service.url =
       "github:darksoil-studio/push-notifications-service/main-0.5";
+    locker.url = "github:darksoil-studio/locker/main-0.5";
 
     aons.url = "github:darksoil-studio/always-online-nodes/main";
     nixos-generators.url = "github:nix-community/nixos-generators";
@@ -65,6 +66,7 @@
             inputs'.scaffolding.packages.hc-scaffold-happ
             inputs'.playground.packages.hc-playground
             inputs'.push-notifications-service.packages.test-push-notifications-service
+            inputs'.locker.packages.test-locker-service
           ];
         };
 
