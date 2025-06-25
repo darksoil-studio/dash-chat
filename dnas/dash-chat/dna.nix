5fd133dc46e729ba021c22236d92aa67a5a8680c
guillem.cordoba <guillem.cordoba@gmail.com>
2025-06-25 13:12:06 +0200

    WIP

--- dnas/dash-chat/dna.nix
+++ dnas/dash-chat/dna.nix
@@ -2,16 +2,10 @@
 
 {
   # Import all ./zomes/coordinator/*/zome.nix and ./zomes/integrity/*/zome.nix  
-  # imports = (
-  #     map (m: "${./.}/zomes/coordinator/${m}/zome.nix")
-  #       (builtins.attrNames (builtins.readDir ./zomes/coordinator))
-  #   )
-  #   ++ 
-  #   (
-  #     map (m: "${./.}/zomes/integrity/${m}/zome.nix")
-  #       (builtins.attrNames (builtins.readDir ./zomes/integrity))
-  #   )
-  # ;
+  imports = (map (m: "${./.}/zomes/coordinator/${m}/zome.nix")
+    (builtins.attrNames (builtins.readDir ./zomes/coordinator)));
+  # ++ (map (m: "${./.}/zomes/integrity/${m}/zome.nix")
+  #   (builtins.attrNames (builtins.readDir ./zomes/integrity)));
   perSystem = { inputs', self', lib, system, ... }: {
     packages.dash_chat_dna =
       inputs.holochain-nix-builders.outputs.builders.${system}.dna {
@@ -23,12 +17,19 @@
           messenger_integrity =
             inputs'.messenger-zome.packages.messenger_integrity;
           messenger = inputs'.messenger-zome.packages.messenger;
-          # Include here the zome packages for this DNA, e.g.:
+
           linked_devices_integrity =
             inputs'.linked-devices-zome.packages.linked_devices_integrity;
           linked_devices = inputs'.linked-devices-zome.packages.linked_devices;
+
           friends_integrity = inputs'.friends-zome.packages.friends_integrity;
           friends = inputs'.friends-zome.packages.friends;
+
+          encrypted_messages = inputs'.locker.packages.encrypted_messages;
+          encrypted_messages_integrity =
+            inputs'.locker.packages.encrypted_messages_integrity;
+
+          locker_async_messages = self'.packages.locker_async_messages;
         };
       };
   };
