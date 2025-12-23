d8ffa2e909591e2a47e5b6c8c563dd641180181e
guillem.cordoba <guillem.cordoba@gmail.com>
2025-12-23 13:14:57 +0100

    Direct message follows correct pattern

--- ui/src/routes/direct-messages/[chatId]/+page.svelte
+++ ui/src/routes/direct-messages/[chatId]/+page.svelte
@@ -3,6 +3,7 @@
 	import '@awesome.me/webawesome/dist/components/avatar/avatar.js';
 	import '@awesome.me/webawesome/dist/components/relative-time/relative-time.js';
 	import '@awesome.me/webawesome/dist/components/format-date/format-date.js';
+	import { m } from '$lib/paraglide/messages.js';
 
 	import { useReactivePromise } from '../../../stores/use-signal';
 	import { lessThanAMinuteAgo, moreThanAnHourAgo } from '../../../utils/time';
@@ -10,7 +11,6 @@
 	import type { ChatsStore, ContactsStore } from 'dash-chat-stores';
 	import { wrapPathInSvg } from '@darksoil-studio/holochain-elements';
 	import { mdiSend } from '@mdi/js';
-	import { m } from '$lib/paraglide/messages.js';
 	import {
 		Page,
 		Navbar,
@@ -20,6 +20,10 @@
 		Card,
 		ListInput,
 		List,
+		Messagebar,
+		ToolbarPane,
+		Icon,
+		useTheme,
 	} from 'konsta/svelte';
 
 	const chatId = window.location.href.split('/').reverse()[0];
@@ -32,125 +36,133 @@
 
 	const messages = useReactivePromise(store.messages);
 	const peerProfile = useReactivePromise(store.peerProfile);
-	let messageInput = $state('');
+	let messageText = $state('');
+	let isClickable = $state(false);
+	let inputOpacity = $state(0.3);
+	const onMessageTextChange = (e: InputEvent) => {
+		messageText = (e.target as HTMLInputElement).value;
+		isClickable = messageText.trim().length > 0;
+		inputOpacity = messageText ? 1 : 0.3;
+	};
 
 	async function sendMessage() {
-		const message = messageInput;
+		const message = messageText;
 		if (!message || message.trim() === '') return;
 
 		await store.sendMessage(message);
-		messageInput = '';
+		messageText = '';
 	}
+	const theme = $derived(useTheme());
 </script>
 
-<Page>
+<Page style={theme === 'material' ? 'height: calc(100vh - 57px)' : ''}>
 	<Navbar transparent={true}>
 		{#snippet left()}
 			<NavbarBackLink onClick={() => (window.location.href = '/')} />
 		{/snippet}
-		{#snippet title()}
-			{#await $peerProfile then profile}
-				<Link
-					href={`/direct-messages/${chatId}/info`}
-					class="gap-2"
-				style="display: flex; align-items: center;"
+		{#await $peerProfile then profile}
+			<div
+				class="gap-2"
+				style="display: flex; justify-content: start; align-items: center; flex: 1"
+			>
+				<wa-avatar
+					image={profile!.avatar}
+					initials={profile!.name.slice(0, 2)}
+					style="--size: 2.5rem"
 				>
-					<wa-avatar
-						image={profile!.avatar}
-						initials={profile!.name.slice(0, 2)}
-						style="--size: 2.5rem"
-					>
-					</wa-avatar>
-					<span>{profile!.name}</span>
-				</Link>
-			{/await}
-		{/snippet}
+				</wa-avatar>
+				<span>{profile!.name}</span>
+			</div>
+		{/await}
 	</Navbar>
 
-	<div
-		class="column center-in-desktop"
-		style="gap: 2rem; flex: 1; margin: 1rem; overflow-y: auto"
-	>
-		<div class="column" style="gap: 1rem; flex: 1">
-			{#await $myActorId then myActorId}
-				{#await $messages then messages}
-					{#each messages as message}
-						{#if myActorId == message.author}
-							<Card
-								class="max-w-[70%] self-end"
-								style="padding: 0.5rem"
-								colors={{ bgIos: 'bg-blue-100', bgMaterial: 'bg-blue-200' }}
-							>
-								<div class="row" style="gap: 0.5rem">
-									<span>{message.content}</span>
+	<div class="column">
+		<div class="center-in-desktop" style="flex:1">
+			<div class="column m-2 gap-2">
+				{#await $myActorId then myActorId}
+					{#await $messages then messages}
+						{#each messages as message}
+							{#if myActorId == message.author}
+								<Card raised class="message my-message">
+									<div class="row gap-2" style="align-items: center">
+										<span>{message.content}</span>
 
-									<div class="quiet text-xs">
-										{#if lessThanAMinuteAgo(message.timestamp)}
-											<span>{m.now()}</span>
-										{:else if moreThanAnHourAgo(message.timestamp)}
-											<wa-format-date
-												hour="numeric"
-												minute="numeric"
-												hour-format="24"
-												date={new Date(message.timestamp)}
-											></wa-format-date>
-										{:else}
-											<wa-relative-time
-												sync
-												format="narrow"
-												date={new Date(message.timestamp)}
-											>
-											</wa-relative-time>
-										{/if}
+										<div class="dark-quiet text-xs">
+											{#if lessThanAMinuteAgo(message.timestamp)}
+												<span>{m.now()}</span>
+											{:else if moreThanAnHourAgo(message.timestamp)}
+												<wa-format-date
+													hour="numeric"
+													minute="numeric"
+													hour-format="24"
+													date={new Date(message.timestamp)}
+												></wa-format-date>
+											{:else}
+												<wa-relative-time
+													sync
+													format="narrow"
+													date={new Date(message.timestamp)}
+												>
+												</wa-relative-time>
+											{/if}
+										</div>
 									</div>
-								</div>
-							</Card>
-						{:else}
-							<Card class="max-w-[70%] self-start" style="padding: 0.5rem">
-								<div class="row" style="gap: 0.5rem">
-									<span>{message.content}</span>
+								</Card>
+							{:else}
+								<div class="row gap-2 m-0">
+									<Card raised class="message others-message">
+										<div class="row gap-2" style="align-items: center">
+											<span>{message.content}</span>
 
-									<div class="quiet text-xs">
-										{#if lessThanAMinuteAgo(message.timestamp)}
-											<span>{m.now()}</span>
-										{:else if moreThanAnHourAgo(message.timestamp)}
-											<wa-format-date
-												hour="numeric"
-												minute="numeric"
-												hour-format="24"
-												date={new Date(message.timestamp)}
-											></wa-format-date>
-										{:else}
-											<wa-relative-time
-												sync
-												format="narrow"
-												date={new Date(message.timestamp)}
-											>
-											</wa-relative-time>
-										{/if}
-									</div>
+											<div class="quiet text-xs">
+												{#if lessThanAMinuteAgo(message.timestamp)}
+													<span>{m.now()}</span>
+												{:else if moreThanAnHourAgo(message.timestamp)}
+													<wa-format-date
+														hour="numeric"
+														minute="numeric"
+														hour-format="24"
+														date={new Date(message.timestamp)}
+													></wa-format-date>
+												{:else}
+													<wa-relative-time
+														sync
+														format="narrow"
+														date={new Date(message.timestamp)}
+													>
+													</wa-relative-time>
+												{/if}
+											</div>
+										</div>
+									</Card>
 								</div>
-							</Card>
-						{/if}
-					{/each}
+							{/if}
+						{/each}
+					{/await}
 				{/await}
-			{/await}
+			</div>
 		</div>
 
-		<div class="row" style="align-items: center; gap: 0.5rem">
-			<List nested>
-				<ListInput
-					type="textarea"
-					bind:value={messageInput}
-					inputStyle={{ minHeight: '40px', maxHeight: '120px' }}
-					class="flex-1"
-					placeholder={m.typeMessage?.() || 'Type a message...'}
-				/>
-			</List>
-
-			<Button clear iconOnly onClick={sendMessage}>
-				<wa-icon src={wrapPathInSvg(mdiSend)}> </wa-icon>
-			</Button>
-		</div>
+		<Messagebar
+			placeholder={m.typeMessage()}
+			onInput={onMessageTextChange}
+			value={messageText}
+		>
+			{#snippet right()}
+				<ToolbarPane class="ios:h-10">
+					<Link
+						iconOnly
+						onClick={() => (isClickable ? sendMessage() : undefined)}
+						style="opacity: {inputOpacity}; cursor: {isClickable
+							? 'pointer'
+							: 'default'}"
+					>
+						<Icon>
+							<wa-icon src={wrapPathInSvg(mdiSend)}> </wa-icon>
+						</Icon>
+					</Link>
+				</ToolbarPane>
+			{/snippet}
+		</Messagebar>
 	</div>
 </Page>
